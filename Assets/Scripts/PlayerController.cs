23e3fa6d4a22a577c6769a1c2ca61d67a82a7c43
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-09-20 13:48:12 -0500

    Detectar el suelo con Raycast

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour{
     public float jumpForce = 6f;
     Rigidbody2D rigidBody;
 
+    public LayerMask groundMask;
+
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -20,12 +22,30 @@ public class PlayerController : MonoBehaviour{
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Jump();  
         }
     }
 
-    void Jump(){
+    void Jump()
+    {
+        if(IsTouchingTheGround())
+        {
         rigidBody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    //Nos indica si el personaje está o no tocando el suelo
+    bool IsTouchingTheGround(){
+        if(Physics2D.Raycast(this.transform.position,
+                             Vector2.down,
+                             2.0f,
+                             groundMask)){
+            //TODO: Programar lógica de contacto con el suelo
+            return true;
+        }else {
+            //TODO: Programar lógica de no contacto
+            return false;
+        }
     }
 }
