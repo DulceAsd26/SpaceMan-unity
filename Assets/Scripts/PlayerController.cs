31267b6699043464ca5c8a22cc56189f03f4ffde
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-09-20 12:44:51 -0500

    Script de control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour{
+    
+    //Variables del movimiento del personaje
+    public float jumpForce = 6f;
+    Rigidbody2D rigidBody;
+    
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+    }
+
+    void Jump(){
+
     }
 }
