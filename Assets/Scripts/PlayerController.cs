aadd6940e31d10bbe7b97f78c861c38ff8242aa5
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-03 10:37:42 -0500

    Zona de muerte

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -78,4 +78,8 @@ public class PlayerController : MonoBehaviour{
             return false;
         }
     }
+    public void Die(){
+        this.animator.SetBool(STATE_ALIVE, false);
+        GameManager.sharedInstance.GameOver();
+    }
 }
