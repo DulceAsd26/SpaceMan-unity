79814e420357f9eed1aa75e93d5f52236c413ce2
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-11-04 08:12:40 -0600

    Programando el menú del juego

--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameView : MonoBehaviour
-{
+public class GameView : MonoBehaviour {
+
+    public Text coinsText, scoreText, maxScoreText;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame){
+            int coins = 0;
+            float score = 0;
+            float maxScore = 0;
+
+            coinsText.text = coins.ToString();
+            scoreText.text = "Score: " + score.ToString("f1");
+            maxScoreText.text = "MaxScore: " + maxScore.ToString("f1");
+        }
     }
 }
