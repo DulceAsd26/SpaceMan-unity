52b511816691c24b7a8bddbcdcfdb8990be4fb28
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-12-01 00:21:17 -0600

    Iniciando la acción de pociones y mana

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,13 @@ public class PlayerController : MonoBehaviour{
     const string STATE_ALIVE = "isAlive";
     const string STATE_ON_THE_GROUND = "isOnTheGround";
 
+
+    private int healthPoints, manaPoints;
+
+    public const int INITIAL_HEALTH = 100, INITIAL_MANA = 15,
+        MAX_HEALTH = 200, MAX_MANA = 30,
+    MIN_HEALTH = 10, MIN_MANA = 0;
+
     public LayerMask groundMask;
 
     void Awake()
@@ -32,6 +39,9 @@ public class PlayerController : MonoBehaviour{
     public void StartGame(){
         animator.SetBool(STATE_ALIVE, true);
         animator.SetBool(STATE_ON_THE_GROUND, true);
+
+        healthPoints = INITIAL_HEALTH;
+        manaPoints = INITIAL_MANA;
 
         Invoke("RestartPosition", 0.2f);
     }
@@ -97,5 +107,25 @@ public class PlayerController : MonoBehaviour{
     public void Die(){
         this.animator.SetBool(STATE_ALIVE, false);
         GameManager.sharedInstance.GameOver();
+    }
+
+
+    public void CollectHealth(int points){
+        this.healthPoints += points;
+        if(this.healthPoints >= MAX_HEALTH){
+            this.healthPoints = MAX_HEALTH;
+        }
+    }
+
+    public void CollectMana(int points){
+        //
+    }
+
+    public int GetHealth(){
+        return healthPoints;
+    }
+
+    public int GetMana(){
+        return manaPoints;
     }
 }
